abab82c4b5409f0eea5ae130c9be3313e5de0e93
Nani <chaos.feature@gmail.com>
2020-05-28 15:10:14 +0300

    calculator

--- pvpcalc/pvp/models.py
+++ pvpcalc/pvp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class game(models.Model):
+    # gem id (url)
+    # status
+    pass
+
+class task(models.Model):
+    # task text
+    # result
+    pass
