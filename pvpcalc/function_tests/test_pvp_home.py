fa1f6ca7bc357bc7ec0be75efec14c1697ed6215
Nani <chaos.feature@gmail.com>
2020-05-29 13:05:14 +0300

    added doc files

--- pvpcalc/function_tests/test_pvp_home.py
+++ pvpcalc/function_tests/test_pvp_home.py
@@ -1,4 +1,13 @@
 from selenium import webdriver
+from pvp.models import Game, Task
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.urls import reverse
+import time
 
 class TestPvpHomePage(StaticLiveServerCase):
-    pass
+
+    def setUp():
+        self.browser =  webdriver.Chrome('function_tests/chtomedriver.exe')
+    
+    def test_foo():
+        pass
